ee8247a92f53f37a4be569c57b5bf3cd9ee70dc6
jkjh8 <jkjh82@naver.com>
2020-12-07 18:22:18 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-card>
+      <v-card-title>
+        <h4>SERVER LIST</h4>
+        <v-spacer></v-spacer>
+        <v-btn
+          icon
+          @click="get"
+        >
+          <v-icon>mdi-refresh</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :items="items"
+        >
+          <template v-slot:item.ip="{ item }">
+            <a
+              :href="'http://'+item.ip+':3000'"
+              target="_blank"
+            >
+              {{ item.ip }}
+            </a>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const ipcRenderer = window.require('electron').ipcRenderer
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  mounted () {
+    ipcRenderer.on('recvIp', (event, value) => {
+      if (Object.prototype.hasOwnProperty.call(value, 'name')) {
+        const result = this.items.every(element => {
+          if (element.ip === value.ip) return false
+          console.log(value)
+        })
+        if (result) {
+          this.items.push(value)
+        }
+      }
+    })
+    this.get()
+  },
+  methods: {
+    get () {
+      ipcRenderer.send('get')
+    }
+  },
+  data () {
+    return {
+      headers: [
+        { text: 'Name', value: 'name' },
+        { text: 'IpAddr', value: 'ip' }
+      ],
+      items: []
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/background.js
@@ -0,0 +1,117 @@
+'use strict'
+
+import { app, protocol, BrowserWindow, Menu, ipcMain } from 'electron'
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+import ip from 'ip'
+
+const PORT = 12345
+const MCAST_ADDR = '230.128.128.128'
+const dgram = require('dgram')
+const server = dgram.createSocket("udp4")
+server.bind(PORT, function () {
+  server.setBroadcast(true)
+  server.setMulticastTTL(128)
+  server.addMembership(MCAST_ADDR)
+})
+
+// Scheme must be registered before the app is ready
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } }
+])
+let win
+async function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // Use pluginOptions.nodeIntegration, leave this alone
+      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      nodeIntegration: true
+    }
+  })
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    if (!process.env.IS_TEST) win.webContents.openDevTools()
+  } else {
+    createProtocol('app')
+    // Load the index.html when not in development
+    win.loadURL('app://./index.html')
+  }
+
+  Menu.setApplicationMenu(null)
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    try {
+      await installExtension(VUEJS_DEVTOOLS)
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString())
+    }
+  }
+  createWindow()
+})
+
+// Exit cleanly on request from parent process in development mode.
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
+        app.quit()
+      }
+    })
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit()
+    })
+  }
+}
+
+
+function brocastGetip () {
+  const rtObject = ({
+    rtip: ip.address()
+  })
+  const message = JSON.stringify(rtObject)
+  server.send(message, 0, message.length, 12345, '230.128.128.128')
+}
+
+server.on('listening', function () {
+  console.log('Multicast Listening = ' + server.address().address + ':' + server.address().port)
+})
+
+server.on('message', function (message, remote) {
+  const msg = JSON.parse(message)
+  console.log(msg)
+  win.webContents.send('recvIp', msg)
+})
+
+ipcMain.on('get', () => {
+  console.log('getip!!!!')
+  brocastGetip()
+})
